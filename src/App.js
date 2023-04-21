ecaca25726a7e8c45eba937a481e91d9da8009bc
Ali <ali.moaydi2020@gmail.com>
2023-04-21 16:13:10 +0430

    1. Create app and Api and Context

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ProductsContextProvider from './context/Products.context.provider';
+
+function App() {
+  return (
+    <ProductsContextProvider>
+      ali
+    </ProductsContextProvider>
+  );
+}
+
+export default App;
---
+++ src/context/Products.context.provider.js
@@ -0,0 +1,20 @@
+import React, { createContext, useEffect, useState } from 'react'
+
+import { getProducts } from '../services/ApiService';
+
+const ProductsContext = createContext();
+
+export default function ProductsContextProvider(props) {
+    const [products , setProducts] = useState([]);
+    useEffect(() => {
+        const fetchApi = async() => {
+            setProducts(await getProducts('/products'))
+        }
+        fetchApi();
+    } , [])
+  return (
+    <ProductsContext.Provider value={products}>
+        {props.children}
+    </ProductsContext.Provider>
+  )
+}
